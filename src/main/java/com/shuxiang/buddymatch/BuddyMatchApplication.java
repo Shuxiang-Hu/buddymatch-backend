11ce677bf6a2d29d44162029a231a12e685d4798
Shuxiang Hu <e0954632@u.nus.edu>
2023-06-26 14:59:58 +0800

    Add printing ip address

--- src/main/java/com/shuxiang/buddymatch/BuddyMatchApplication.java
+++ src/main/java/com/shuxiang/buddymatch/BuddyMatchApplication.java
@@ -7,6 +7,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 @MapperScan("com.shuxiang.buddymatch.mapper")
 @EnableOpenApi
@@ -16,6 +19,16 @@ public class BuddyMatchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BuddyMatchApplication.class, args);
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            String ipAddress = inetAddress.getHostAddress();
+            String hostName = inetAddress.getHostName();
+
+            System.out.println("Application IP address: " + ipAddress);
+            System.out.println("Application host name: " + hostName);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 
 }
